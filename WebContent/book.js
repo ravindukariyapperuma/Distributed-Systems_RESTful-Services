11a93283ecbcf811e454a5dd73e2ebfda9487715
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-14 18:24:34 +0530

    deleteDetails method added book.js

--- WebContent/book.js
+++ WebContent/book.js
@@ -22,3 +22,43 @@ function getDetails(){
         timeout: 120000,
     });
 };
+
+function addDetails(){
+	
+	var book = {
+            id: $("#newid").val(),
+            title:$("#title").val(),
+            price:$("#price").val()
+        }
+	
+	
+	jQuery.ajax({
+        url: "http://localhost:8080/BookService/rest/books/",
+        type: "POST",
+        contentType: "application/json",  
+        dataType:'json',
+        data: JSON.stringify(book),
+        success: function(data, textStatus, errorThrown) {
+        	 $("#span").html(data);
+        },
+    });
+};
+
+
+function deleteDetails(){
+	jQuery.ajax({
+        url: "http://localhost:8080/BookService/rest/books/" + $("#id").val(),
+        type: "DELETE",
+        contentType: "application/json",  
+        dataType:'json',
+        success: function(data, textStatus, errorThrown) {
+        	 $("#span").html(data);
+
+        },
+        error : function(jqXHR, textStatus, errorThrown) {
+        		$("#span").text("Sorry! Book can not delete!");
+        		
+        },
+        timeout: 120000,
+    });
+};
