232f38bb1d59a16bdfbd03af3446e24e8a608d98
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-14 18:04:52 +0530

    complete for search books

--- WebContent/book.js
+++ WebContent/book.js
@@ -1,3 +1,24 @@
 /**
  * 
  */
+
+function getDetails(){
+	jQuery.ajax({
+        url: "http://localhost:8080/book_service/rest/books/" + $("#id").val(),
+        type: "GET",
+        contentType: "application/json",  
+        dataType:'json',
+        success: function(data, textStatus, errorThrown) {
+            //here is your json.
+              // process it
+        	  $("#title").text(data.title);
+        	  $("#price").text(data.price);
+
+        },
+        error : function(jqXHR, textStatus, errorThrown) {
+        		$("#title").text("Sorry! Book not found!");
+        		$("#price").text("");
+        },
+        timeout: 120000,
+    });
+};
